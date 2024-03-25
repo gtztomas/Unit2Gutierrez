3de7436e0da8edfe9840f357ec7f5c628602fb1f
Tomas Gutierrez <723369@student.dusd.net>
2024-03-25 15:26:39 -0700

    Finish 2.3-2.4

--- Unit2GutierrezP8/Assets/Scripts/PlayerController.cs
+++ Unit2GutierrezP8/Assets/Scripts/PlayerController.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
    public float horizontalInput;
-    public float speed = 10.0f;
-    public float xRange = 10;
+    public float speed = 15.0f;
+    public float xRange = 20;
 
     public GameObject projectilePrefab;
+
+    public float zMin;
+    public float zMax;
+    public float verticalInput;
     
     // Start is called before the first frame update
     void Start()
@@ -29,8 +33,24 @@ public class PlayerController : MonoBehaviour
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
+
+        if (transform.position.z < zMin) 
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zMin);
+        }
+       
+        if (transform.position.z > zMax) 
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zMax);
+        }
+
         horizontalInput = Input.GetAxis("Horizontal"); 
         transform.Translate(Vector3.right *  horizontalInput * Time.deltaTime * speed);
+
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
+
+
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
